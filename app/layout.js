e0c50dfb89aa919a51cc47459da99c6416260332
thisisthesarath <149189766+thisisthesarath@users.noreply.github.com>
2024-07-13 14:56:11 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,18 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-50RJG4PPLT"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-50RJG4PPLT');
-          `}
-        </Script>
+        <link rel="icon" href="/icon.png" type="image/png" />
       </head>
       <body>{children}</body>
     </html>
