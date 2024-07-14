8e42989900df1eb66be68a3bc3f13f3b268cc244
proclub-srcas <srcasprogrammingclub@gmail.com>
2024-07-14 21:31:06 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+                <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KFK599LN04"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KFK599LN04');
+        </script>
         <link rel="icon" href="/icon.png" type="image/png" />
       </head>
       <body>{children}</body>
