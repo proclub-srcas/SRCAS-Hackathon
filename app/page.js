d115082f36327e6f746e8728015373e35e374d63
thisisthesarath <149189766+thisisthesarath@users.noreply.github.com>
2024-07-31 14:26:10 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -317,6 +317,10 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Registration endS on 31st July 2024! REGISTER SOON!</h1>
+        <ScrollingText />
+      </div>
       
       <div className="bg-black py-10">
         <h2 className="text-white text-center text-3xl md:text-4xl font-bold mb-10">Schedule</h2>
