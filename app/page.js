d4ca7d5ee59a070f7b8b861ea13a93f954a11796
proclub-srcas <srcasprogrammingclub@gmail.com>
2024-08-06 14:17:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -190,7 +190,7 @@ export default function Home() {
   return (
    <div className="overflow-x-hidden">
       <Navbar />
-      <PopupAd />
+    //  <PopupAd />
       <div
         className="relative flex flex-col min-h-fit p-4 pt-20 md:pt-32 md:px-12 xl:px-20 md:py-4"
       >
